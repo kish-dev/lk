563b6ffceb55278aed2e37f86c4af17fae1eae3a
Anton Gulyaev <kishmyak2001@gmail.com>
2021-06-10 17:10:42 +0300

    [Contacts] - some changes

--- app/src/main/java/com/example/lktsu/ContactsActivity.kt
+++ app/src/main/java/com/example/lktsu/ContactsActivity.kt
@@ -1,32 +1,48 @@
 package com.example.lktsu
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.ContactsContract
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.lktsu.data.Id
+import com.example.lktsu.data.model.StudentEntity
+import com.example.lktsu.di.DataStoreScope
+import com.example.lktsu.repositories.RoomRepositoryImpl
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.*
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class ContactsActivity : AppCompatActivity() {
+    @Inject
+    lateinit var roomRepositoryImpl: RoomRepositoryImpl
+    private lateinit var student: StudentEntity
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contacts)
 
+        runBlocking(Dispatchers.IO) {
+            student = roomRepositoryImpl.getStudent(Id.string)
+        }
+
         val tel1: TextView = findViewById(R.id.Phone1)
-        tel1.setText("89586639724") // из таблицы student поле tel1 по id = 172016
+        tel1.text = student.phone1// из таблицы student поле tel1 по id = 172016
 
         val tel2: TextView = findViewById(R.id.Phone2)
-        tel2.setText("89651589140") // из таблицы student поле tel2 по id = 172016
+        tel2.text =  student.phone2 // из таблицы student поле tel2 по id = 172016
 
         val tel3: TextView = findViewById(R.id.Phone2)
-        tel3.setText("") // из таблицы student поле tel3 по id = 172016
+        tel3.text =  student.phone3// из таблицы student поле tel3 по id = 172016
 
         val email: TextView = findViewById(R.id.EmailAddress)
-        email.setText("madeeva.va@gmail.com") // из таблицы student поле email по id = 172016
+        email.text =  student.email// из таблицы student поле email по id = 172016
     }
 
     fun onClickInfo(view: View?) {
-        val text = "Для изменения контактных данных введите новую информацию в соответсвующее поле и нажмите кнопку Сохранить"
+        val text =
+            "Для изменения контактных данных введите новую информацию в соответсвующее поле и нажмите кнопку Сохранить"
         val duration = Toast.LENGTH_SHORT
 
         val toast = Toast.makeText(applicationContext, text, duration)
